adf9b4e2b4e02708d8377c21a3c5a19083c7b65c
tristyn <trisclassie@gmaiil.com>
2022-09-14 04:45:56 -0600

    Challenge 1 and 2 done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    getDogs()
+
+})
+
+function getDogs(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    return fetch(imgUrl)
+    .then(response => response.json())
+    .then(response => { 
+        const dogImageContainer = document.getElementById("dog-image-container")
+        response.message.forEach(url => {
+            const img = document.createElement("img")
+            img.src = url
+            dogImageContainer.append(img)
+        })
+    }) 
+    
+}
+
+function getBreeds() {
+
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl) 
+    .then(response => response.json())
+    .then(response => {
+        const breeds = Object.keys(response.message)
+        const ul = document.querySelector("#dog-breeds")
+        breeds.map(breed => {
+            const li = document.createElement("li")
+            li.textContent = breed
+            ul.append(li)
+
+
+        })    })
+}
+
+
+
+
+getDogs()
+getBreeds()
