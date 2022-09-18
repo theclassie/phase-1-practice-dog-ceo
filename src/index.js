93d499c00d1a0d002d2458f3d438f3b503b6d8c1
tristyn <trisclassie@gmaiil.com>
2022-09-18 05:56:07 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+let breeds = []
 
 document.addEventListener("DOMContentLoaded", () => {
     getDogs()
@@ -6,8 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function getDogs(){
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
-    return fetch(imgUrl)
+    fetch(imgUrl)
     .then(response => response.json())
     .then(response => { 
         const dogImageContainer = document.getElementById("dog-image-container")
@@ -20,25 +21,41 @@ function getDogs(){
     
 }
 
-function getBreeds() {
 
+
+function getBreeds() {
     const breedUrl = 'https://dog.ceo/api/breeds/list/all'
     fetch(breedUrl) 
     .then(response => response.json())
     .then(response => {
-        const breeds = Object.keys(response.message)
-        const ul = document.querySelector("#dog-breeds")
-        breeds.map(breed => {
-            const li = document.createElement("li")
-            li.textContent = breed
-            ul.append(li)
-
-
-        })    })
+        breeds = Object.keys(response.message)
+        addBreedsToDom(breeds)
+        })    
 }
 
+function addBreedsToDom(breeds) {
+    const ul = document.querySelector("#dog-breeds")
+    breeds.map(breed => {
+        const li = document.createElement("li")
+        li.textContent = breed
+        ul.append(li)
+    })
+}
+    document.addEventListener("click", event => {
+        if(event.target.matches("li")) {
+            event.target.style.color = "orange"
+        }
 
+    })
 
+    document.addEventListener("change", event => {
+        if(event.target.matches("#breed-dropdown")) {
+        const ul = document.querySelector("#dog-breeds")
+        ul.innerHTML = ''
+        const filterBreeds = breeds.filter(breed => breed[0] === event.target.value)
+        addBreedsToDom(filterBreeds)
+    }
+    })
 
 getDogs()
 getBreeds()
