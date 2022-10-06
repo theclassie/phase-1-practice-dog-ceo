4a70e9a2addc1e1be681588b6aa6629cd7609ce8
Tristyn Broadhead <trisclassie@gmail.com>
2022-10-06 09:40:54 -0600

    commit tree fa0e3f6d1aff114fc781fde117c945ea7257cdd6

--- src/index.js
+++ src/index.js
@@ -1,61 +1,14 @@
-
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
-let breeds = []
+const randomDogImg = "https://dog.ceo/api/breeds/image/random/4"
 
 document.addEventListener("DOMContentLoaded", () => {
-    getDogs()
-
+    fetchRandomDogImg()
 })
 
-function getDogs(){
-    fetch(imgUrl)
-    .then(response => response.json())
-    .then(response => { 
-        const dogImageContainer = document.getElementById("dog-image-container")
-        response.message.forEach(url => {
-            const img = document.createElement("img")
-            img.src = url
-            dogImageContainer.append(img)
-        })
-    }) 
-    
+fetchRandomDogImg(){
+    fetch(randomDogImg)
+    .then(res => res.json)
+    .then(res => 
+    //adds image to the DOM
+    document.getElementById("dog-image-container")
+    )
 }
-
-
-
-function getBreeds() {
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    fetch(breedUrl) 
-    .then(response => response.json())
-    .then(response => {
-        breeds = Object.keys(response.message)
-        addBreedsToDom(breeds)
-        })    
-}
-
-function addBreedsToDom(breeds) {
-    const ul = document.querySelector("#dog-breeds")
-    breeds.map(breed => {
-        const li = document.createElement("li")
-        li.textContent = breed
-        ul.append(li)
-    })
-}
-    document.addEventListener("click", event => {
-        if(event.target.matches("li")) {
-            event.target.style.color = "orange"
-        }
-
-    })
-
-    document.addEventListener("change", event => {
-        if(event.target.matches("#breed-dropdown")) {
-        const ul = document.querySelector("#dog-breeds")
-        ul.innerHTML = ''
-        const filterBreeds = breeds.filter(breed => breed[0] === event.target.value)
-        addBreedsToDom(filterBreeds)
-    }
-    })
-
-getDogs()
-getBreeds()
